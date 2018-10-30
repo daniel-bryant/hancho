c0b488467d6b9dc219f16848ec51bd8bef701e8b
daniel-bryant <daniel.bryant@checkr.com>
2018-10-29 17:42:53 -0700

    Refactor commands into Command struct

--- command.go
+++ command.go
@@ -2,26 +2,59 @@ package main
 
 import (
   "fmt"
+  "strings"
 )
 
-func handleCommand(command string) {
-  const unknown = "hancho: '%s' is not a hancho command. See 'hancho help'.\n"
+type Command struct {
+  Names []string
+  Handle func()
+  Description string
+}
 
-  const usage = `usage: hancho <command>
+var availableCommands = []Command{
+  {[]string{"up", "u"}, handleStartCommand, "Create and start all docker containers"},
+  {[]string{"down", "d"}, handleStopCommand, "Stop and remove all containers"},
+}
 
-With <command> being one of the following:
+func handleHelpCommand() {
+  fmt.Println("Usage: hancho <command>\n")
+  fmt.Println("With <command> being one of the following:\n")
+  printCommands()
+}
 
-  start, s   Start proxy server (default)
-  help, h    Show help`
+func printCommands() {
+  max := 0
+  for _, command := range availableCommands {
+    length := len(strings.Join(command.Names,", "))
+    if length > max {
+      max = length
+    }
+  }
 
-  switch command {
-  case "start", "s", "up", "":
-    handleStartCommand()
-  case "stop", "down":
-    handleStopCommand()
-  case "help", "h":
-    fmt.Println(usage)
-  default:
-    fmt.Printf(unknown, command)
+  for _, command := range availableCommands {
+    names := strings.Join(command.Names,", ")
+    padding := strings.Repeat(" ", max - len(names))
+    fmt.Println(names, padding, command.Description)
+  }
+}
+
+func handleCommand(arg string) {
+  helpCommand := Command{[]string{"help", "h"}, handleHelpCommand, "Show help"}
+  availableCommands = append(availableCommands, helpCommand)
+
+  for _, command := range availableCommands {
+    for _, name := range command.Names {
+      if arg == name {
+        command.Handle()
+        return
+      }
+    }
+  }
+
+  if len(arg) == 0 {
+    handleHelpCommand()
+  } else {
+    fmt.Printf("'%s' is not a hancho command. See 'hancho help'.\n\n", arg)
+    printCommands()
   }
 }
